780ba54c3f23da8fde5c651d98f8db470447cd78
Mitchell Andrews <mitchellw.andrews@gmail.com>
2018-12-17 22:59:09 -0800

    updated database schema and seed data

--- spikes/main-app-boilerplate/docker/postgres/init.sql
+++ spikes/main-app-boilerplate/docker/postgres/init.sql
@@ -1,6 +1,36 @@
-CREATE TABLE notes(
+CREATE TABLE participants(
   id SERIAL PRIMARY KEY,
-  note VARCHAR(255)
+  first_name VARCHAR(255),
+  middle_name VARCHAR(255),
+  last_name VARCHAR(255),
+  aka VARCHAR(255) ARRAY,
+  status VARCHAR(100),
+  dob DATE,
+  phone VARCHAR(10),
+  email VARCHAR(255),
+  address VARCHAR(255),
+  age VARCHAR(3),
+  ethnicity VARCHAR(20),
+  race VARCHAR(20),
+  gender VARCHAR(20),
+  income_source VARCHAR(255),
+  income_range VARCHAR(255),
+  family_status VARCHAR(20),
+  housing_status VARCHAR(255),
+  chronic_homeless BOOLEAN,
+  veteran_status VARCHAR(255),
+  urgent BOOLEAN,
+  services VARCHAR(255) [],
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
  );
 
-INSERT INTO notes (note) VALUES ('testnote');
+ CREATE TABLE citations(
+   id SERIAL PRIMARY KEY,
+   citation_number VARCHAR(50),
+   court_code VARCHAR(50),
+   violation_number VARCHAR(50),
+   citation_status VARCHAR(255),
+   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   participant_id INTEGER,
+   FOREIGN KEY (participant_id) REFERENCES participants (id)
+ );
---
+++ spikes/main-app-boilerplate/docker/postgres/seed.sql
@@ -0,0 +1,72 @@
+INSERT INTO participants (
+  first_name,
+  middle_name,
+  last_name,
+  aka,
+  status,
+  dob,
+  phone,
+  email,
+  address,
+  age,
+  ethnicity,
+  race,
+  gender,
+  income_source,
+  income_range,
+  family_status,
+  housing_status,
+  chronic_homeless,
+  veteran_status,
+  urgent,
+  services)
+VALUES (
+  'Mitchell',
+  'W',
+  'Andrews',
+  ARRAY['Mitch'],
+  'status',
+  DATE('8-6-1990'),
+  '1231231234',
+  'email@email.com',
+  '123 some st',
+  '28',
+  'caucasian',
+  'white',
+  'male',
+  'work',
+  '1-1000000',
+  'single',
+  'rent',
+  false,
+  'not veteran',
+  false,
+  ARRAY['service1', 'service2']);
+
+  INSERT INTO citations (
+    citation_number,
+    court_code,
+    violation_number,
+    citation_status,
+    participant_id
+  ) VALUES (
+    '1123-321',
+    'CC 1234',
+    'VN 1234',
+    'open',
+    1
+  );
+
+  INSERT INTO citations (
+    citation_number,
+    court_code,
+    violation_number,
+    citation_status,
+    participant_id
+  ) VALUES (
+    '1123-322',
+    'CC 1235',
+    'VN 1235',
+    'warrant',
+    1
+  )
