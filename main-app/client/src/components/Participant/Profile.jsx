3b3a3773b273c5c252fbbcb143f321ade258af70
serpient <littlegangwolf@gmail.com>
2019-02-20 20:29:32 -0800

    move api call into isolated function

--- main-app/client/src/components/Participant/Profile.jsx
+++ main-app/client/src/components/Participant/Profile.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { API_ENDPOINT } from './../../get_uri';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import Loader from '../UI/Loader';
 import Error from '../UI/Error';
 import './Profile.scss';
-const axios = require('axios');
+import getParticipant from './getParticipant.api';
 
 class ParticipantProfile extends React.Component {
   constructor(props) {
@@ -17,26 +16,22 @@ class ParticipantProfile extends React.Component {
     }
   }
   componentDidMount() {
-    let { id } = this.props.match.params;
-    axios({
-      method: 'get',
-      url: `${API_ENDPOINT}/participants/${id}`,
-      timeout: 2000,
-    })
-    .then(res => {
-      let { data } = res;
-      this.setState({ user: data[0], loading: false });
-      return data[0];
-    })
-    .catch(err => {
-      console.error(err);
-      let { message } = err;
-      if (err.code === 'ECONNABORTED') {
-        message = 'The request took too long - please try again later.';
-      }
-      this.setState({ error: message, loading: false });
-      return err;
-    })
+    if (this.props.match && this.props.match.params.id) {
+      let { id } = this.props.match.params;
+      return this.getParticipant(id);
+    } else {
+      this.onError('Please add a participant ID to the route.')
+    }
+  }
+  getParticipant = (id) => {
+    this.setState({ error: null, loading: true });
+    return getParticipant(id, this.onSuccess, this.onError);
+  }
+  onSuccess = (data) => {
+    this.setState({ user: data, loading: false })
+  }
+  onError = (errorMessage) => {
+    this.setState({ error: errorMessage, loading: false });
   }
   render() {
     let { user, loading, error } = this.state;
@@ -44,7 +39,7 @@ class ParticipantProfile extends React.Component {
         <div className='user-profile--container'>
           { loading && <Loader /> }
           { error && <Error error={error} /> }
-          <Link to={'/participants'} className='user-profile--nav'>Back to Index</Link>
+          <a href={'/participants'} className='user-profile--nav'>Back to Index</a>
           { user && <Card user={user} /> }
         </div>
     )
---
+++ main-app/client/src/components/Participant/getParticipant.api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { API_ENDPOINT } from './../../get_uri';
+
+const getParticipant = (id, successFn, errorFn) => {
+  // return axios({
+  //   method: 'get',
+  //   url: `${API_ENDPOINT}/participants/${id}`,
+  //   timeout: 2000,
+  // })
+  return axios.get(`${API_ENDPOINT}/participants/${id}`, {
+    timeout: 3000,
+  })
+  .then(res => {
+    let { data } = res;
+    successFn(data[0]);
+    return res;
+  })
+  .catch(err => {
+    console.error(err);
+    let { message } = err;
+    if (err.code === 'ECONNABORTED') {
+      message = 'The request took too long - please try again later.';
+    }
+    errorFn(message);
+    return err;
+  })
+}
+
+export default getParticipant;
