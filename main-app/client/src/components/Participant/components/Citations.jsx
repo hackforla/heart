0a58fbff06571fa8384f66b1dc7603370ae750e5
serpient <littlegangwolf@gmail.com>
2019-02-21 13:11:10 -0800

    import forms into Citations

--- main-app/client/src/components/Participant/components/Citations.jsx
+++ main-app/client/src/components/Participant/components/Citations.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { DynamicFormContainer } from 'components/DynamicForm';
+import CitationsQA from './Citations.data';
 class Citations extends React.Component {
   render() {
     return (
-      
+      <DynamicFormContainer questions={CitationsQA} />
     )
   }
 }
+
+export default Citations;
