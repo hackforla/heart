fcc788bda96d21b2f12e170e29a90b090e32f7f1
serpient <littlegangwolf@gmail.com>
2019-02-20 19:19:12 -0800

    put browserRouter in App

--- main-app/client/src/App.js
+++ main-app/client/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ParticipantProfile from './components/Participant/Profile';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          <Switch>
-            <Route exact={true} path='/participants/:id/' component={ParticipantProfile} />
-            <Route path='/participants' component={ParticipantProfile} />
-          </Switch>
-      </div>
+    return (  
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path='/participants/:id/' component={ParticipantProfile} />
+          <Route path='/participants' component={ParticipantProfile} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
