235f26d8a283c821d4b68040938f8e16821cc065
serpient <littlegangwolf@gmail.com>
2019-02-20 17:14:31 -0800

    move browserRouter to wrap App

--- main-app/client/src/App.js
+++ main-app/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ParticipantProfile from './components/Participant/Profile';
 import './App.css';
 
@@ -7,12 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
           <Switch>
             <Route path='/participants/:id/' component={ParticipantProfile} />
             <Route path='/participants' component={ParticipantProfile} />
           </Switch>
-        </BrowserRouter>
       </div>
     );
   }
