373ead8e61ab403422d9a8999aecfe515bb5302e
Tien Yuan <tien.yuan@gmail.com>
2019-03-04 20:41:43 -0800

    omg it reads teh file

--- main-app/client/src/components/Import/FromFile.jsx
+++ main-app/client/src/components/Import/FromFile.jsx
@@ -1,47 +1,76 @@
 import React from 'react';
+import Papa from 'papaparse';
 import { API_ENDPOINT } from './../../get_uri';
 const axios = require('axios');
+
 
 class ImportFromFile extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      file: null,
+      csvfile: null,
       error: null,
       loading: true,
     }
-  }
-  componentDidMount() {
+    this.updateData = this.updateData.bind(this);
   }
 
-  // handleFileRead = () => {
-  //   console.log(`i have read`);
-  // };
-
-  handleFileChosen = (e) => {
-      console.log(`i have chosen`);
-      let fileReader = new FileReader();
-      // console.log(fileReader);
-      // fileReader.onloadend = handleFileRead;
-      // fileReader.readAsText(e);
+  handleChange = event => {
+    this.setState({
+      csvfile: event.target.files[0]
+    });
   };
 
-  render() {
-    let { file, loading, error } = this.state;
-    return (
-      <div className='file--container'>
-        { loading && <div id='loader'><i className='fas fa-spinner'></i></div> }
-        { file && file }
-        <input type='file'
-               id='file'
-               className='input-file'
-               accept='.csv'
-             />
-        <input type="submit" onClick={e => this.handleFileChosen(e)}/>
-      </div>
-    )
+  importCSV = () => {
+    const { csvfile } = this.state;
+    Papa.parse(csvfile, {
+      complete: this.updateData,
+      header: true
+    });
+  };
+
+  updateData(result) {
+    var data = result.data;
+    console.log(data);
   }
+
+  render() {
+    console.log(this.state.csvfile);
+    return (
+      <div className="ImportFromFile">
+        <h2>Import CSV File!</h2>
+        <input
+          className="csv-input"
+          type="file"
+          ref={input => {
+            this.filesInput = input;
+          }}
+          name="file"
+          placeholder={null}
+          onChange={this.handleChange}
+        />
+        <p />
+        <button onClick={this.importCSV}> Upload now!</button>
+      </div>
+    );
+  }
+
+  // render() {
+  //   let { file, loading, error } = this.state;
+  //   return (
+  //     <div className='file--container'>
+  //       { loading && <div id='loader'><i className='fas fa-spinner'></i></div> }
+  //       { file && file }
+  //       <input type='file'
+  //              id='file'
+  //              className='input-file'
+  //              accept='.csv'
+  //            />
+  //       <input type="submit" onClick={e => this.handleFileChosen(e)}/>
+  //     </div>
+  //   )
+  // }
 }
 
 export default ImportFromFile;
