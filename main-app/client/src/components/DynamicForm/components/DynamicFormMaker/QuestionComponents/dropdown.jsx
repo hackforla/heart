c997fb89a1771794c28b408feaafcbbb01bfa8b2
serpient <littlegangwolf@gmail.com>
2019-03-11 19:25:31 -0700

    render multiple dropdown values in its own elements; add change handler to remove values

--- main-app/client/src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/dropdown.jsx
+++ main-app/client/src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/dropdown.jsx
@@ -23,34 +23,64 @@ const dropdown = (
       isMultiValues.push({ label: elem, value: elem });
     });
   }
+  const changeHandler = target => {
+    let value = !target ? "" : target.value;
+    if (isMulti) {
+      value = target.map(elem => {
+        return elem.value;
+      });
+    }
+    const currentTarget = {
+      value,
+      name: field_name,
+      type: input_type
+    };
+    return onFormChange({ currentTarget });
+  };
+
+  const changeHandlerForMulti = (e, valueToBeDeleted) => {
+    e.preventDefault();
+    for (var i = 0; i < isMultiValues.length; i++) {
+      if (isMultiValues[i].value === valueToBeDeleted) {
+        isMultiValues.splice(i, 1);
+      }
+    }
+    return changeHandler(isMultiValues);
+  };
   return (
-    <Select
-      styles={customStyles}
-      placeholder={placeholder}
-      escapeClearsValue={true}
-      isClearable={true}
-      isSearchable={true}
-      name={field_name}
-      options={mappedOptions}
-      value={isMulti ? isMultiValues : { label, value }}
-      classNamePrefix="react-dropdown"
-      isMulti={isMulti}
-      isDisabled={!editable}
-      onChange={target => {
-        let value = !target ? "" : target.value;
-        if (isMulti) {
-          value = target.map(elem => {
-            return elem.value;
-          });
-        }
-        const currentTarget = {
-          value,
-          name: field_name,
-          type: input_type
-        };
-        return onFormChange({ currentTarget });
-      }}
-    />
+    <>
+      {isMulti && (
+        <div className="dropdown--multiple-container">
+          {isMultiValues.map((value, idx) => {
+            return (
+              <div key={idx} className="dropdown--multiple-labels">
+                {value.label}
+                <button
+                  onClick={e => changeHandlerForMulti(e, value.label)}
+                  className="dropdown--multiple-btn"
+                >
+                  X
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <Select
+        styles={customStyles}
+        placeholder={placeholder}
+        escapeClearsValue={true}
+        isClearable={true}
+        isSearchable={true}
+        name={field_name}
+        options={mappedOptions}
+        value={isMulti ? isMultiValues : { label, value }}
+        classNamePrefix="react-dropdown"
+        isMulti={isMulti}
+        isDisabled={!editable}
+        onChange={target => changeHandler(target)}
+      />
+    </>
   );
 };
 
