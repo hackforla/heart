7a33af4e75fd411cbda50106c2340576247a7f5b
serpient <littlegangwolf@gmail.com>
2019-03-18 19:55:08 -0700

    add state, and onSuccess and onError, and formSubmit handlers

--- main-app/client/src/components/Participant/components/Citations.jsx
+++ main-app/client/src/components/Participant/components/Citations.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import { DynamicFormContainer } from 'components/DynamicForm';
-import CitationsQA from './Citations.data';
-import './Citations.scss';
+import React from "react";
+import { DynamicFormContainer } from "components/DynamicForm";
+import CitationsQA from "./Citations.data";
+import "./Citations.scss";
 class Citations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: false,
+    }
+  }
+  onSuccess = (data) => {
+    this.initializeFormState(data);
+    this.setState({ loading: false })
+  }
+  onError = (errorMessage) => {
+    this.setState({ error: errorMessage, loading: false });
+  }
+  postFormData = (id, formData) => {
+    this.setState({ loading: true, error: null });
+    return updateParticipant({ id, data: formData }, this.onSuccess, this.onError);
+  }
   render() {
     return (
-      <section className='citations-container'>
-        <div className='citations-title'>Citations</div>
-        <div className='citations-form'>
-          <DynamicFormContainer questions={CitationsQA} editable={true} />
+      <section className="citations-container">
+        <div className="citations-title">Citations</div>
+        <div className="citations-form">
+          <DynamicFormContainer questions={CitationsQA} editableMode={true} onSubmit={} />
         </div>
       </section>
-    )
+    );
   }
 }
 
