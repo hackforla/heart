28a61f6fcc1748cd188f2a993ad4c3c35892d1a6
serpient <littlegangwolf@gmail.com>
2019-03-24 15:01:33 -0700

    add a multi dropdown question to mock QA

--- main-app/client/src/components/DynamicForm/tests/mockQA.js
+++ main-app/client/src/components/DynamicForm/tests/mockQA.js
@@ -1,73 +1,86 @@
 export const question_set_1 = [
-    {
-        category_name: '',
-        category_contents: [
+  {
+    category_name: "",
+    category_contents: [
+      {
+        row: [
           {
-            row: [
-              {
-                text: 'Clinic Attended',
-                input_type: 'text',
-                field_name: 'clinic_attended',
-                placeholder: 'location',
-              },
-            ]
+            text: "Clinic Attended",
+            input_type: "text",
+            field_name: "clinic_attended",
+            placeholder: "location"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    category_name: "Personal Information",
+    category_contents: [
+      {
+        row: [
+          {
+            text: "First Name",
+            input_type: "text",
+            field_name: "first_name",
+            placeholder: "First Name"
           }
         ]
       },
       {
-        category_name: 'Personal Information',
-        category_contents: [
-          {
-            row: [
-              {
-                text: 'First Name',
-                input_type: 'text',
-                field_name: 'first_name',
-                placeholder: 'First Name',
-              },
-            ],
-          },
-          {
-            text: 'Also known as',
-            input_type: 'text',
-            field_name: 'known_as',
-            placeholder: 'Other names they go by',
-          },
-        ],
-      },
+        text: "Also known as",
+        input_type: "text",
+        field_name: "known_as",
+        placeholder: "Other names they go by"
+      }
+    ]
+  },
+  {
+    category_name: "Contact Information",
+    category_contents: [
       {
-        category_name: 'Contact Information',
-        category_contents: [
-          {
-            text: 'Email Address (optional)',
-            input_type: 'email',
-            field_name: 'email',
-            placeholder: 'email@emailaddress.com',
-            optional: true,
-          },
-        ]
-      },
-    {
-        text: 'Age',
-        input_type: 'dropdown',
-        field_name: 'age',
-        placeholder: 'Choose Age Range',
-        options: [
-          { text: '18-24' },
-          { text: '25-54' },
-          { text: '55-61' },
-          { text: '62-Older' },
-        ]
-      },
-      {
-        text: 'Ethnicity',
-        input_type: 'dropdown',
-        field_name: 'ethnicity',
-        placeholder: 'Choose Ethnicity',
-        options: [
-          { text: 'Hispanic/Latino' },
-          { text: 'Not Hispanic/Latino' },
-          { text: 'Unknown' },
-        ]
-      },
-]
+        text: "Email Address (optional)",
+        input_type: "email",
+        field_name: "email",
+        placeholder: "email@emailaddress.com",
+        optional: true
+      }
+    ]
+  },
+  {
+    text: "Age",
+    input_type: "dropdown",
+    field_name: "age",
+    placeholder: "Choose Age Range",
+    options: [
+      { text: "18-24" },
+      { text: "25-54" },
+      { text: "55-61" },
+      { text: "62-Older" }
+    ]
+  },
+  {
+    text: "Ethnicity",
+    input_type: "dropdown",
+    field_name: "ethnicity",
+    placeholder: "Choose Ethnicity",
+    options: [
+      { text: "Hispanic/Latino" },
+      { text: "Not Hispanic/Latino" },
+      { text: "Unknown" }
+    ]
+  },
+  {
+    text: "Violations",
+    input_type: "dropdown-multi",
+    field_name: "violations",
+    placeholder: "Ex.PC 123.4",
+    isMulti: true,
+    options: [
+      { text: "VC 4000", description: "Registration Required" },
+      { text: "VC 4152.5", description: "Foreign Vehicle Registration" },
+      { text: "VC 4159", description: "Notice of Change of Address" },
+      { text: "VC 4454", description: "Registration Card Kept With Vehicle" }
+    ]
+  }
+];
