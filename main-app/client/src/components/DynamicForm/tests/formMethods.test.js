ca8c676fab545a56c79b00c4d65afa1c4089f0e2
serpient <littlegangwolf@gmail.com>
2019-03-24 14:46:32 -0700

    add first test for dynamic form

---
+++ main-app/client/src/components/DynamicForm/tests/formMethods.test.js
@@ -0,0 +1,13 @@
+import { _getDefaultFormData } from '../components/formMethods';
+import { question_set_1 } from './mockQA';
+
+it('creates default form data', () => {
+    expect(_getDefaultFormData(question_set_1)).toEqual({
+        clinic_attended: '',
+        first_name: '',
+        known_as: '',
+        email: '',
+        age: '18-24',
+        ethnicity: 'Hispanic/Latino',
+    })
+});
