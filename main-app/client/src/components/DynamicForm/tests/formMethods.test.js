8aaef80a2576998d5ba99139dff142dd1d3b1fa8
serpient <littlegangwolf@gmail.com>
2019-03-24 17:31:57 -0700

    add test validation flow

--- main-app/client/src/components/DynamicForm/tests/formMethods.test.js
+++ main-app/client/src/components/DynamicForm/tests/formMethods.test.js
@@ -91,4 +91,86 @@ describe("Dynamic Form Validation", () => {
       SubmitBtnState.ENABLED
     );
   });
+
+  it("Correct input change of all required fields, after multiple changes", () => {
+    initialFormData.first_name = "Frank"; // required
+    initialFormData.email = "test@gmail.com"; // required
+    initialFormData.location = ["San Francisco", "Los Angeles"]; // required
+
+    initialValidation.first_name_is_valid = true;
+    initialValidation.email_is_valid = true;
+    initialValidation.location_is_valid = true;
+
+    expect(_validateAllAnswers(initialFormData, question_set_1)).toEqual({
+      fields_is_valid: initialValidation
+    });
+    expect(_determineSubmitBtnState(initialValidation)).toEqual(
+      SubmitBtnState.ENABLED
+    );
+  });
+});
+
+describe("Dynamic Form Validation Flow", () => {
+  let initialFormData, initialValidation;
+  beforeAll(() => {
+    initialFormData = {
+      clinic_attended: "",
+      first_name: "",
+      known_as: "",
+      email: "",
+      age: "18-24",
+      ethnicity: "Hispanic/Latino",
+      violations: [],
+      location: []
+    };
+
+    initialValidation = {
+      clinic_attended_is_valid: true,
+      first_name_is_valid: false, // required
+      known_as_is_valid: true,
+      email_is_valid: false, // required
+      age_is_valid: true,
+      ethnicity_is_valid: true,
+      violations_is_valid: true,
+      location_is_valid: false // required
+    };
+  });
+  it("Correct input change of all required fields", () => {
+    initialFormData.first_name = "Frank"; // required
+    initialFormData.email = "test@gmail.com"; // required
+    initialFormData.location = ["San Francisco", "Los Angeles"]; // required
+
+    initialValidation.first_name_is_valid = true;
+    initialValidation.email_is_valid = true;
+    initialValidation.location_is_valid = true;
+
+    expect(_validateAllAnswers(initialFormData, question_set_1)).toEqual({
+      fields_is_valid: initialValidation
+    });
+    expect(_determineSubmitBtnState(initialValidation)).toEqual(
+      SubmitBtnState.ENABLED
+    );
+  });
+
+  it("Disabled submit btn after invalidating required field", () => {
+    initialFormData.first_name = "";
+    initialValidation.first_name_is_valid = false;
+    expect(_validateAllAnswers(initialFormData, question_set_1)).toEqual({
+      fields_is_valid: initialValidation
+    });
+    expect(_determineSubmitBtnState(initialValidation)).toEqual(
+      SubmitBtnState.DISABLED
+    );
+  });
+
+  it("Disabled submit btn after invalidating required field", () => {
+    initialFormData.first_name = "Alexa";
+    initialValidation.first_name_is_valid = true;
+    expect(_validateAllAnswers(initialFormData, question_set_1)).toEqual({
+      fields_is_valid: initialValidation
+    });
+    expect(_determineSubmitBtnState(initialValidation)).toEqual(
+      SubmitBtnState.ENABLED
+    );
+  });
 });
