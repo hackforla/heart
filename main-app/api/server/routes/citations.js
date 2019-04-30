d2ce1f5ec58a63204a9a240f75d001155cc6146f
Shlomi Benshlomo <shlomibe@hotmail.com>
2019-04-29 20:00:19 -0700

    Fixed 2 merge issues

--- main-app/api/server/routes/citations.js
+++ main-app/api/server/routes/citations.js
@@ -4,12 +4,6 @@ const knex = require('../config/knex_config.js');
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
-  app.post('/citations', jwtAuth, (req, res) => {
-    knex('citations').insert(req.body)
-      .then(() => res.status(200).send())
-      .catch(err => res.status(500).send(err));
-  });
-
   app.put('/citations/:id', jwtAuth, (req, res) => {
     knex('citations')
       .where('id', req.params.id)
