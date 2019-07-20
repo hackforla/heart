72f769db9bfd9bd27bd2380e3cf00d94b84b756e
Matt Moeller <ultamatt@gmail.com>
2019-07-20 15:45:55 -0700

    Working on getting real participants into the UI

--- main-app/client/src/components/ParticipantsList/TableRow.js
+++ main-app/client/src/components/ParticipantsList/TableRow.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-const TableRow = ({ record }) => {
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+const TableRow = ({ record, header_fields }) => {
+  console.log(header_fields)
   return (
     <tr>
-      {Object.keys(record).map((key, index) => (
-        <td key={index}>{record[key]}</td>
-      ))}
-      <td>
-        <Link to="/participants/1">
-          <span className="view-profile-link">view profile</span>
-        </Link>
-      </td>
+      {Object.keys(header_fields).map((key, index) =>
+        header_fields[key].special === 'link' ? (
+          <td>
+            <Link to="/participants/{record.id}">
+              <span className="view-profile-link">view profile</span>
+            </Link>
+          </td>
+        ) : (
+          <td key={index}>{record[key]}</td>
+        )
+      )}
     </tr>
-  );
-};
+  )
+}
 
 TableRow.propTypes = {
   record: PropTypes.object,
-};
+  fields: PropTypes.object,
+}
 
-export default TableRow;
+export default TableRow
