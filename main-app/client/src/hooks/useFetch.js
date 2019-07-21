333b06028bd357011cc35568499b400b4abba9f9
abregorivas <abregorivas@gmail.com>
2019-07-20 20:18:14 -0700

    update fetch hook as reducer for full crud

--- main-app/client/src/hooks/useFetch.js
+++ main-app/client/src/hooks/useFetch.js
@@ -1,35 +1,103 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from 'axios';
 import { UserAuth } from '../utilities/auth';
 import { API_BASE_URL } from '../config/url_config'
 
-const useFetch = (url) =>{
-  const [config] =useState(() => {
-    return {headers: { Authorization: `Bearer ${UserAuth.getAuthToken()}` }}
-  })
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  async function fetchUrl() {
-    const response = await axios.get(`${API_BASE_URL}/${url}`, config, {
-      timeout: 3000,
-    })
-      .then(res => res)
-      .catch(err => {
-        if (err.code === 'ECONNABORTED') {
-          err.message = 'The request took too long - please try again later.';
-        }
-        return err;
-      });
-
-    setData(response);
-    setLoading(false);
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return {
+        data: action.payload,
+        isLoading: false,
+        isError: false,
+        initialLoad: false
+      };
+    case 'ADD_DATA':
+      return {
+        data: state.data.concat(action.payload),
+        isLoading:false,
+        isError: false
+      };
+    case 'UPDATE_DATA':
+      return {
+        data: state.data.map(x => x.id === action.id ? action.payload[0] : x),
+        isLoading: false,
+        isError: false,
+      };
+    case 'DELETE_DATA':
+      return state.filter(x => x.id !== action.id);
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error(`Unhandled type: ${action.type}`);
   }
+};
 
-  useEffect(() => {
-    fetchUrl();
-  }, []);
-  return [data, loading];
-}
+const useFetch = (url) =>{
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    initialLoad: true, isLoading: true, isError: false, data: []
+  });
+
+  const fetchData = (method, url, formData, headers) => {
+    return axios({
+      method: method,
+      url: `${API_BASE_URL}/${url}`,
+      data: formData ? formData: null,
+      timeout: method === 'get' ? 3000 : 5000,
+      headers: { Authorization: `Bearer ${UserAuth.getAuthToken()}` }
+    })
+      .then(res => {
+        switch (method) {
+          case 'get':
+            dispatch({type: 'FETCH_DATA', payload: res.data});
+            break;
+          case 'post':
+            dispatch({type: 'ADD_DATA', payload: formData});
+            break;
+          case 'put':
+            // need to review what backend will actually use (should be an id)
+            dispatch({type: 'UPDATE_DATA', payload: res.data.data, id: formData.id});
+            break;
+          case 'delete':
+            // need to review what backend will actually use (should be an id)
+            dispatch({type: 'DELETE_DATA', id: formData.Id});
+            break;
+          default:
+            dispatch({})
+        }
+      })
+      .catch(error => {
+        if(error.code === 'ECONNABORTED') {
+          error.message = 'The request took too long - please try again later.'
+        }
+        dispatch({ type: 'FETCH_FAILURE', payload: error});
+      })
+  };
+
+  useEffect (() => {
+    // common problem in React that component state is set even though the
+    // component got already unmounted (e.g. due to navigating away with React Router).
+    // "didCancel" flag addresses that issue
+
+    let didCancel = false;
+    if (!didCancel) {
+      if(state.initialLoad) {
+        fetchData('get', url);
+      } else {
+        fetch()
+      }
+    }
+
+    // clean up when component un-mounts
+    return () => didCancel = true;
+
+  },[url]);
+
+  return [state, fetchData, dispatch]
+};
 
 export { useFetch };
