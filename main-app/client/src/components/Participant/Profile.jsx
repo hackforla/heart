b0a09f9a82c7f4ef7a2ffa35ee4e98de95df630a
abregorivas <abregorivas@gmail.com>
2019-07-21 01:17:13 -0700

    update profile component to use Mat-ui

--- main-app/client/src/components/Participant/Profile.jsx
+++ main-app/client/src/components/Participant/Profile.jsx
@@ -1,37 +1,69 @@
 import React from 'react'
+import { Container, Grid, Link } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import Card from './components/Card'
 import Loader from '../UI/Loader'
 import Error from '../UI/Error'
-import './Profile.scss'
 import Citations from './components/Citations'
 import Notes from './components/Notes'
 import Status from './components/Status'
-import { Page, PageContent } from 'components/UI/Page'
-import {useFetch} from '../../hooks/useFetch'
+import { useFetch } from '../../hooks/useFetch'
 
-const ParticipantProfile = (props) => {
-  const [state, fetchData, dispatch] = useFetch(`participants/${props.match.params.id}`);
+const useStyles = makeStyles(theme => ({
+  root: { padding: theme.spacing(2), backgroundColor: '#f3f7fc' },
+  link: {
+    color: "#252525",
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    fontSize: 12,
+    fontWeight: 600,
+    '&:hover': {
+      color: '#428FE2',
+      textDecoration: 'none'
+    },
+  },
+    arrowLeft: {
+      display: 'inline-block',
+      marginBottom: theme.spacing(1),
+      marginLeft: theme.spacing(1)
+    },
 
+}))
+
+const ParticipantProfile = props => {
+  const [state, fetchData, dispatch] = useFetch(
+    `participants/${props.match.params.id}`
+  )
+  const classes = useStyles()
   return (
-    <Page blue>
-      <PageContent>
-        {state.isLoading && <Loader />}
-        {state.isError && <Error error={state.isError} />}
-        {!state.isLoading && state.data && (
-          <div className="user-profile--content-container">
-            <a href={'/participants'} className="user-profile--nav">
-              <i className="fas fa-arrow-left"></i>
-              Back to Index
-            </a>
-            <Card user={state.data[0]} />
-            <Notes user={state.data[0]} />
-            <Citations user={state.data[0]} />
-            <Status user={state.data[0]} />
-          </div>
-        )}
+    <div className={classes.root}>
+      {state.isLoading ? (
+        <Loader />
+      ) : (
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Link to="/participants" className={classes.link}>
+                {state.isError && <Error error={state.isError} />}
+                <i className="fas fa-arrow-left"></i>
+                <span className={classes.arrowLeft}>Back to Index</span>
+              </Link>
+            </Grid>
+            <Grid item xs={12}>
+              <Card user={state.data[0]} />
+            </Grid>
+            <Grid item xs={8}>
+              <Notes user={state.data[0]} />
+              <Citations user={state.data[0]} />
+            </Grid>
+            <Grid item xs={4}>
+              <Status user={state.data[0]} />
+            </Grid>
+          </Grid>
+        </Container>
+      )}
       {JSON.stringify(state)}
-      </PageContent>
-    </Page>
+    </div>
   )
 }
 
