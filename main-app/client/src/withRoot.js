690f1dad08ef355ad28c62d8f8f031bb75f4485b
abregorivas <abregorivas@gmail.com>
2019-07-20 19:15:48 -0700

    remove app.css & clean up theme file

--- main-app/client/src/withRoot.js
+++ main-app/client/src/withRoot.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 
