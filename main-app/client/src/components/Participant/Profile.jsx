03d99a83be51a1e345276ca0211a3f24b1c95489
abregorivas <abregorivas@gmail.com>
2019-07-20 20:18:35 -0700

    refactor Profile comp to use new fetch hook

--- main-app/client/src/components/Participant/Profile.jsx
+++ main-app/client/src/components/Participant/Profile.jsx
@@ -3,62 +3,36 @@ import Card from './components/Card'
 import Loader from '../UI/Loader'
 import Error from '../UI/Error'
 import './Profile.scss'
-import getParticipant from 'api/getParticipant.api'
 import Citations from './components/Citations'
 import Notes from './components/Notes'
 import Status from './components/Status'
 import { Page, PageContent } from 'components/UI/Page'
+import {useFetch} from '../../hooks/useFetch'
 
-class ParticipantProfile extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: null,
-      error: null,
-      loading: true,
-    }
-  }
-  componentDidMount() {
-    if (this.props.match && this.props.match.params.id) {
-      let { id } = this.props.match.params
-      return this.getParticipant(id)
-    } else {
-      this.onError('Please add a participant ID to the route.')
-    }
-  }
-  getParticipant = id => {
-    this.setState({ error: null, loading: true })
-    return getParticipant(id, this.onSuccess, this.onError)
-  }
-  onSuccess = data => {
-    this.setState({ user: data, loading: false })
-  }
-  onError = errorMessage => {
-    this.setState({ error: errorMessage, loading: false })
-  }
-  render() {
-    let { user, loading, error } = this.state
-    return (
-      <Page blue>
-        <PageContent>
-          {loading && <Loader />}
-          {error && <Error error={error} />}
-          {!loading && user && (
-            <div className="user-profile--content-container">
-              <a href={'/participants'} className="user-profile--nav">
-                <i className="fas fa-arrow-left"></i>
-                Back to Index
-              </a>
-              <Card user={user} />
-              <Notes user={user} />
-              <Citations user={user} />
-              <Status user={user} />
-            </div>
-          )}
-        </PageContent>
-      </Page>
-    )
-  }
+const ParticipantProfile = (props) => {
+  const [state, fetchData, dispatch] = useFetch(`participants/${props.match.params.id}`);
+
+  return (
+    <Page blue>
+      <PageContent>
+        {state.isLoading && <Loader />}
+        {state.isError && <Error error={state.isError} />}
+        {!state.isLoading && state.data && (
+          <div className="user-profile--content-container">
+            <a href={'/participants'} className="user-profile--nav">
+              <i className="fas fa-arrow-left"></i>
+              Back to Index
+            </a>
+            <Card user={state.data[0]} />
+            <Notes user={state.data[0]} />
+            <Citations user={state.data[0]} />
+            <Status user={state.data[0]} />
+          </div>
+        )}
+      {JSON.stringify(state)}
+      </PageContent>
+    </Page>
+  )
 }
 
 export default ParticipantProfile
