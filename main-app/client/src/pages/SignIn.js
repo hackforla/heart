3700b83a1cd741f635d6f67a0b644ca0e2d2f6a3
Shlomi Benshlomo <shlomibe@hotmail.com>
2019-07-29 21:50:05 -0700

    Added a new folder 'pages' to the client folder and 5 new files for our pages.

---
+++ main-app/client/src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function SignInPage(props) {
+  return (
+    <div className="signin-page-container">
+      <h1 className="signin-page">Intake Page</h1>
+    </div>
+  )
+}
+
+export default SignInPage
