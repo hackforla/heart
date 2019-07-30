991863cb4d244f7fae4c74b45b8ea111a0231efd
Tabatahg <Tabatahg@gmail.com>
2019-07-30 14:36:47 -0700

    fix typos, back to index button, and add red asterik

--- main-app/client/src/components/Form/IntakeForm/components/ClinicFormGroup.js
+++ main-app/client/src/components/Form/IntakeForm/components/ClinicFormGroup.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { Field } from 'formik'
 import '../style/FormGroup.scss'
 
+const starStyle = {
+  color: '#f44336',
+}
+
 const ClinicFormGroup = props => {
   return (
     <div className="clinic-form-group">
+      <div className="title" style={starStyle}>
+        * = Required
+      </div>
       <div className="title">Clinic Attended</div>
 
       <div className="form-inputs">
-        <label className="label">Date</label>
+        <label className="label">
+          Date<span style={starStyle}>*</span>
+        </label>
         <Field
+          required
           type="date"
           name="clinic_attended"
           placeholder="location"
@@ -18,8 +28,12 @@ const ClinicFormGroup = props => {
         />
       </div>
       <div className="form-inputs">
-        <label className="label">Referral Source</label>
+        <label className="label">
+          Referral Source
+          <span style={starStyle}>*</span>
+        </label>
         <Field
+          required
           type="text"
           name="referral_sourced"
           placeholder="Referral Source"
