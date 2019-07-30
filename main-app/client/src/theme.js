1afffb4c1a26648b514b3f6803ca63cdcfc044e9
abregorivas <abregorivas@gmail.com>
2019-07-30 00:03:16 -0700

    add material-ui icons & fix lint error on theme

--- main-app/client/src/theme.js
+++ main-app/client/src/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
-const font = "'Work Sans', sans-serif";
+const font = "'Work Sans', sans-serif"
 
 function pxToRem(value) {
   return `${value / 16}rem`
