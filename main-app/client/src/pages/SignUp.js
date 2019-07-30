808ced240512f7383bf151af30c00a411b7595c0
Shlomi Benshlomo <shlomibe@hotmail.com>
2019-07-29 21:50:05 -0700

    Added a new folder 'pages' to the client folder and 5 new files for our pages.

---
+++ main-app/client/src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function SignUpPage(props) {
+  return (
+    <div className="signup-page-container">
+      <h1 className="signup-page">SignUp Page</h1>
+    </div>
+  )
+}
+
+export default SignUpPage
