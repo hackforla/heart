a174a4d0e8cf4567ade74777c237520460bf3e83
abregorivas <abregorivas@gmail.com>
2019-08-01 04:33:52 -0700

    add sorting utility functions file

---
+++ main-app/client/src/utilities/sortingUtils.js
@@ -0,0 +1,27 @@
+//performs sort and orders by a property
+
+export function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+export function getSorting(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy)
+}
+
+export function stableSort(array, cb) {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = cb(a[0], b[0])
+    if (order !== 0) return order
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map(el => el[0])
+}
