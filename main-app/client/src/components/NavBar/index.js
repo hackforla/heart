d472f9e8971096ad05101fd0a7babc887fa511d6
abregorivas <abregorivas@gmail.com>
2019-08-03 16:47:14 -0700

    cleanup styles & remove loginBtn on Navbar

--- main-app/client/src/components/NavBar/index.js
+++ main-app/client/src/components/NavBar/index.js
@@ -1,39 +1,35 @@
 import React from 'react'
 
-import { UserAuth } from '../../utilities/auth'
-
 import { PATHS } from '../../routes'
 import { withRouter } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 
 import { AppBar, Toolbar, Button } from '@material-ui/core'
 
+import AccountCircle from '@material-ui/icons/AccountCircle'
 const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing(3), // only used when nav is fixed
+    flexGrow: 1,
+  },
   ToolBar: {
     display: 'flex',
     justifyContent: 'space-between',
-    [theme.breakpoints.down('xs')]: {
-      display: 'block',
-    },
   },
   LeftContent: {
-    marginLeft: '9%',
-    [theme.breakpoints.down('xs')]: {
-      margin: 'auto',
-    },
+    width: '100%',
   },
   RightContent: {
-    marginRight: '9%',
-    [theme.breakpoints.down('xs')]: {
-      margin: 'auto',
-    },
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
   },
-  Button: {
-    width: 'auto',
-    margin: '5px',
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
+  user: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    marginRight: theme.spacing(2),
   },
 }))
 
@@ -45,12 +41,11 @@ const NavBar = ({ onLogout }) => {
       if (onClick) {
         onClick()
       }
-
       history.push(path)
     }
 
     return withRouter(({ history }) => (
-      <Button className={classes.Button} onClick={handleClick(history)}>
+      <Button classes={{ root: classes.button }} onClick={handleClick(history)}>
         {name}
       </Button>
     ))
@@ -68,41 +63,25 @@ const NavBar = ({ onLogout }) => {
     onClick: onLogout,
     path: PATHS.LOGIN,
   })
-  const LogInButton = buttonWithRoute({
-    name: 'Login',
-    path: PATHS.LOGIN,
-  })
 
   return (
-    <div>
-      <AppBar color="default" position="sticky">
+    <nav className={classes.root}>
+      <AppBar color="default">
         <Toolbar className={classes.ToolBar}>
-          {UserAuth.loggedIn() ? (
-            <>
-              <div className={classes.LeftContent}>
-                <HomeButton />
-                <IntakeFormButton />
-              </div>
-              <div className={classes.RightContent}>
-                <LogOutButton />
-                <span>
-                  Gina <i className="fas fa-user-circle"></i>
-                </span>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className={classes.LeftContent}>
-                <p>Heart</p>
-              </div>
-              <div className={classes.RightContent}>
-                <LogInButton />
-              </div>
-            </>
-          )}
+          <div className={classes.LeftContent}>
+            <HomeButton />
+            <IntakeFormButton />
+          </div>
+
+          <div className={classes.RightContent}>
+            <LogOutButton />
+            <div className={classes.user}>
+              Gina <AccountCircle />
+            </div>
+          </div>
         </Toolbar>
       </AppBar>
-    </div>
+    </nav>
   )
 }
 
