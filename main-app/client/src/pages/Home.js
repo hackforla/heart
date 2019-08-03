71a12edbff2bdd30d96d3b9c6806251623cabbf0
abregorivas <abregorivas@gmail.com>
2019-08-03 02:43:12 -0700

    update homepage

--- main-app/client/src/pages/Home.js
+++ main-app/client/src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
-import ParticipantList from '../components/ParticipantsList/ParticipantsList'
+import Participants from '../components/ParticipantsList/Participants/Participants'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles({
+  root: { height: 'calc(100vh - 64px)' },
+})
 
 export const HomePage = () => {
+  const classes = useStyles()
   return (
-    <Container>
-      <h1 className="home-page">Home Page</h1>
-      <ParticipantList />
+    <Container className={classes.root}>
+      <Participants />
     </Container>
   )
 }
