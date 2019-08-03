eb9c36dc017b8aa9d8918ddc9b22e1ca17bdb589
abregorivas <abregorivas@gmail.com>
2019-08-03 02:54:08 -0700

    refactor HeaderFields api

--- main-app/client/src/components/ParticipantsList/Participants/tableHeadersApi.js
+++ main-app/client/src/components/ParticipantsList/Participants/tableHeadersApi.js
@@ -1,24 +1,44 @@
-export const headers = [
-  { as: 'Rush', fields: ['urgent'], format: 'icon', sortable: true },
-  { as: 'Date', fields: ['created_at'], format: 'date', sortable: true },
-  {
-    as: 'Referral Source',
-    fields: ['referral_source'],
-    format: 'icon',
+export const headers = {
+  urgent: {
+    as: 'Rush',
+    fields: ['urgent'],
+    format: 'string',
     sortable: true,
   },
-  {
+  created_at: {
+    as: 'Date',
+    fields: ['created_at'],
+    format: 'date',
+    sortable: true,
+  },
+  referral_source: {
+    as: 'Referral Source',
+    fields: ['referral_source'],
+    format: 'string',
+    sortable: true,
+  },
+  name: {
     as: 'Name',
     fields: ['first_name', 'last_name'],
     format: 'string',
     sortable: true,
   },
-  { as: 'Case Status', fields: ['status'], format: 'string', sortable: true },
-  {
+  status: {
+    as: 'Case Status',
+    fields: ['status'],
+    format: 'string',
+    sortable: true,
+  },
+  updated_at: {
     as: 'Last Update',
     fields: ['updated_at'],
     format: 'dateTime',
     sortable: true,
   },
-  { as: '', fields: ['id'], format: 'string', sortable: false },
-]
+  id: {
+    as: '',
+    fields: ['id'],
+    format: 'string',
+    sortable: false,
+  },
+}
