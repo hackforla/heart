23a8bb1a43db949e4698876673420fe28cc72563
abregorivas <abregorivas@gmail.com>
2019-08-03 17:05:42 -0700

    moved formUtils & Form Tabs to Form Feature

--- main-app/client/src/components/Form/FormTabs/index.jsx
+++ main-app/client/src/components/Form/FormTabs/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props
 
   return (
     <Typography
@@ -21,20 +21,20 @@ function TabPanel(props) {
     >
       <Box p={3}>{children}</Box>
     </Typography>
-  );
+  )
 }
 
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
   value: PropTypes.any.isRequired,
-};
+}
 
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
-  };
+  }
 }
 
 const useStyles = makeStyles(theme => ({
@@ -42,25 +42,26 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
-}));
+}))
 
 const FormTabs = ({ forms }) => {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
 
-  const renderTabs = (forms) => forms.map(({ label }, i) => {
-    return <Tab key={i} label={label} {...a11yProps(i)} />
-  });
+  const renderTabs = forms =>
+    forms.map(({ label }, i) => {
+      return <Tab key={i} label={label} {...a11yProps(i)} />
+    })
 
-  const renderForms = (forms) => forms.map(({ Form, props }, i) => (
-    <TabPanel key={i} index={i} value={value}>
-      <Form {...props} />
-    </TabPanel>
-  ));
-
+  const renderForms = forms =>
+    forms.map(({ Form, props }, i) => (
+      <TabPanel key={i} index={i} value={value}>
+        <Form {...props} />
+      </TabPanel>
+    ))
 
   function handleChange(event, newValue) {
-    setValue(newValue);
+    setValue(newValue)
   }
 
   return (
@@ -68,14 +69,17 @@ const FormTabs = ({ forms }) => {
       <AppBar position="static">
         <Tabs
           variant="fullWidth"
-          value={value} onChange={handleChange} aria-label="Simple tabs example">
+          value={value}
+          onChange={handleChange}
+          aria-label="Simple tabs example"
+        >
           {renderTabs(forms)}
         </Tabs>
       </AppBar>
       {renderForms(forms)}
     </div>
-  );
-};
+  )
+}
 
 FormTabs.propTypes = {
   forms: PropTypes.arrayOf(
@@ -84,16 +88,14 @@ FormTabs.propTypes = {
       label: PropTypes.string.isRequired,
 
       // can either be a class component or functional component
-      Form: PropTypes.oneOfType([
-        PropTypes.elementType,
-        PropTypes.func
-      ]).isRequired,
+      Form: PropTypes.oneOfType([PropTypes.elementType, PropTypes.func])
+        .isRequired,
 
       // optional props that'll be passed to the form component
       // when rendered
-      props: PropTypes.object
+      props: PropTypes.object,
     })
-  )
-};
+  ),
+}
 
-export default FormTabs;
+export default FormTabs
