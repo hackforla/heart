dd9d5bed698bc098bc283531eef468a3548d9fe0
abregorivas <abregorivas@gmail.com>
2019-08-06 23:46:04 -0700

    move InfoItem to shared folder

---
+++ main-app/client/src/components/ParticipantProfile/shared/InfoItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Typography } from '@material-ui/core'
+
+export const InfoItem = ({ ...props }) => {
+  return (
+    <Grid item {...props}>
+      <Typography component="h3" variant="overline" color="textSecondary">
+        {props.title}
+      </Typography>
+      <Typography variant="caption" color="textPrimary">
+        {props.desc || 'N/A'}
+      </Typography>
+    </Grid>
+  )
+}
+
+InfoItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+}
+
+InfoItem.defaultProps = {
+  title: '',
+  desc: '',
+}
+
+export default InfoItem
