70fc519dedc77738092db5f25e07948ce5fde33c
abregorivas <abregorivas@gmail.com>
2019-08-18 22:52:37 -0700

    add status (closing) form with new db fields

--- main-app/client/src/components/ParticipantProfile/Status/CaseClosedForm.js
+++ main-app/client/src/components/ParticipantProfile/Status/CaseClosedForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Field } from 'formik'
-import { BaseCheckBox, BaseTextArea, FormGroupCheckBox } from '../FormElements'
+import { BaseCheckBox, FormGroupCheckBox } from '../FormElements'
 import uuid from 'uuid'
 
 const CaseClosedForm = ({ value, touched, onChange, onBlur, disabled }) => {
   return (
     <div>
       <FormGroupCheckBox
-        id="background_check"
+        id="case_closed_reason"
         label="Reason for Closing"
         value={value}
         touched={touched}
@@ -25,23 +25,23 @@ const CaseClosedForm = ({ value, touched, onChange, onBlur, disabled }) => {
           <Field
             key={uuid()}
             component={BaseCheckBox}
-            name="background_check"
+            name="case_closed_reason"
             id={x}
             label={x}
             disabled={disabled}
           />
         ))}
-        <Field
-          disabled={disabled}
-          name="reason"
-          id="reason"
-          type="text"
-          multiline
-          label="Reason"
-          variant="outlined"
-          inputProps={{ 'aria-label': 'reason-comments' }}
-          component={BaseTextArea}
-        />
+        {/*<Field*/}
+        {/*  disabled={disabled}*/}
+        {/*  name="case_closed_other"*/}
+        {/*  id="case_closed_other"*/}
+        {/*  type="text"*/}
+        {/*  multiline*/}
+        {/*  label="Reason"*/}
+        {/*  variant="outlined"*/}
+        {/*  inputProps={{ 'aria-label': 'reason-comments' }}*/}
+        {/*  component={BaseTextArea}*/}
+        {/*/>*/}
       </FormGroupCheckBox>
     </div>
   )
