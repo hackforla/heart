50f8194ac5ea591b8eb4715a97c83f13108fd090
abregorivas <abregorivas@gmail.com>
2019-08-25 14:48:11 -0700

    create index.js export for intake formGroups

---
+++ main-app/client/src/components/Intake/FormGroups/index.js
@@ -0,0 +1,5 @@
+export { ClinicFormGroup } from './ClinicFormGroup'
+export { ContactInfoFormGroup } from './ContactInfoFormGroup'
+export { GeneralInfoFormGroup } from './GeneralInfoFormGroup'
+export { FamilyAndIncomeFormGroup } from './FamilyAndIncomeFormGroup'
+export { AgreementsFormGroup } from './AgreementsFormGroup'
