5a566b67d73d01438ae7c4a3ed79844abe4d433f
abregorivas <abregorivas@gmail.com>
2019-08-26 14:27:01 -0700

    rename Question comp to QuestionCheckBox

--- main-app/client/src/components/Intake/FormGroups/AgreementsFormGroup.js
+++ main-app/client/src/components/Intake/FormGroups/AgreementsFormGroup.js
@@ -1,37 +1,23 @@
 import React from 'react'
-import { Field } from 'formik'
 import { makeStyles, Typography, Box, Divider } from '@material-ui/core'
 import FormSection from '../FormSection'
-import { BaseCheckBox } from '../../ParticipantProfile/FormElements'
-
+import QuestionCheckBox from '../QuestionCheckBox'
 const useStyles = makeStyles(theme => ({
   agreements: {
-    padding: '20px 0 10px 0',
-  },
-  field: {
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
   },
 }))
 
-const Question = ({ name, label }) => {
-  const classes = useStyles()
-  return (
-    <Box className={classes.field}>
-      <Field component={BaseCheckBox} name={name} label={label} />
-    </Box>
-  )
-}
-
 export const AgreementsFormGroup = ({ ...props }) => {
   const classes = useStyles()
   return (
     <FormSection heading="Agreements">
-      <Question
+      <QuestionCheckBox
         name="agreements_obligations.agreement_voluntary"
         label="I am taking part of this program voluntarily"
       />
-      <Question
+      <QuestionCheckBox
         name="agreements_obligations.agreement_at_risk"
         label="I am currently homeless or at risk of homelessness"
       />
@@ -40,15 +26,15 @@ export const AgreementsFormGroup = ({ ...props }) => {
         <Typography gutterBottom variant="h6">
           I understand that...
         </Typography>
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_obligations"
           label="in order to take part of this program, I will need to complete X obligations"
         />
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_infractions"
           label="Only infractions are eligible to be expunged and that misdemeanors and felonies are not eligible to be expunged through this program"
         />
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_warrants"
           label="Any active warrants in my name will block any expungement that I could have obtained through the program"
         />
@@ -58,11 +44,11 @@ export const AgreementsFormGroup = ({ ...props }) => {
         <Typography gutterBottom variant="h6">
           I understand that...
         </Typography>
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_court_form"
           label="This is not a court form, and that I cannot bring this form to court for any proceedings I have been asked to appear to"
         />
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_progress"
           label="This program can take up to 3 or 4 months to take effect, and that I can check up on my cases's progress at anytime by contacting xxxxxxxxx"
         />
@@ -73,18 +59,18 @@ export const AgreementsFormGroup = ({ ...props }) => {
           I give the HEART team permission to follow up with me on my
           application's progress, the best way to contact me is
         </Typography>
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_phone_contact"
           label="phone"
         />
-        <Question
+        <QuestionCheckBox
           name="agreements_obligations.agreement_email_contact"
           label="email"
         />
       </Box>
       <Divider />
       <Box>
-        <Question
+        <QuestionCheckBox
           name="participants.urgent"
           label="Is this an urgent case? 🔥🔥🔥"
         />
