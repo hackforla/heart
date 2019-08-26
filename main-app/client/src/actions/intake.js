daf583cf71de16379f0ade1708d0f9337c3f6494
abregorivas <abregorivas@gmail.com>
2019-08-26 14:33:42 -0700

    remove bg color on Intake page

--- main-app/client/src/actions/intake.js
+++ main-app/client/src/actions/intake.js
@@ -2,7 +2,23 @@ import axios from 'axios'
 import { API_BASE_URL } from '../config/url_config'
 import { UserAuth } from '../utilities/auth'
 
-export const addParticipant = (data) => {
+export const addParticipant = data => {
+  const authToken = UserAuth.getAuthToken()
+  let config = {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  }
+  return axios
+    .post(`${API_BASE_URL}/participants/`, { data, timeout: 5000 }, config)
+    .then(res => {
+      console.log(res.data[0]['id'])
+      return res.data[0]['id']
+    })
+    .catch(err => err)
+}
+
+export const addAgreementsObligations = (id, data) => {
   const authToken = UserAuth.getAuthToken()
   let config = {
     headers: {
@@ -11,7 +27,7 @@ export const addParticipant = (data) => {
   }
   return axios
     .post(
-      `${API_BASE_URL}/participants/`,
+      `${API_BASE_URL}/participants/${id}/agreements_obligations`,
       { data, timeout: 5000 },
       config
     )
